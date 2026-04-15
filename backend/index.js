8736235fa7bab24f597543caa0cce3ab6e24ed5d
ChaurasiyaDharmendra <golu78dk@gmail.com>
2026-04-15 22:42:46 +0530

    fix cors

--- backend/index.js
+++ backend/index.js
@@ -1,29 +1,3 @@
-// import express from "express"
-// import dotenv from "dotenv"
-// dotenv.config()
-// import connectDb from "./config/db.js"
-// import authRouter from "./routes/auth.routes.js"
-// import cors from "cors"
-// import cookieParser from "cookie-parser"
-// import userRouter from "./routes/user.routes.js"
-// //import geminiResponse from "./gemini.js"
-// //import {geminiResponse} from "./gemini.js"
-
-
-
-
-// const port=process.env.PORT || 5000
-// app.use(express.json())
-// app.use(cookieParser())
-// app.use("/api/auth",authRouter)
-// app.use("/api/user",userRouter)
-
-
-// app.listen(port,()=>{
-//     connectDb()
-//     console.log("server started")
-// })
-
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
@@ -41,12 +15,15 @@ const app = express();
 app.use(
   cors({
     origin: [
-      "http://localhost:5173", // local frontend
-      "https://smart-voice-ai-assistant.vercel.app" // 👈 baad me frontend deploy hoga
+      "http://localhost:5173",
+      "https://smart-voice-ai-assistant.vercel.app"
     ],
+    methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
 );
+
+app.options("*", cors()); // 🔥 IMPORTANT
 
 /* ================= MIDDLEWARE ================= */
 app.use(express.json());
